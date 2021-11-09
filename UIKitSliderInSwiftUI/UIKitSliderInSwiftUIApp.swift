66e0fd6b1ca314e3f1d24e1180d8f560c6082910
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-11-09 09:46:08 +0300

    Initial Commit

---
+++ UIKitSliderInSwiftUI/UIKitSliderInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitSliderInSwiftUIApp.swift
+//  UIKitSliderInSwiftUI
+//
+//  Created by Александр on 9.11.21.
+//
+
+import SwiftUI
+
+@main
+struct UIKitSliderInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
